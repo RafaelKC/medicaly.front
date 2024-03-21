73ffe04d0a0a1bf807405cc8979134bcfbf7c00d
Luis Otavio Silva Santos <76752591+luis-ota@users.noreply.github.com>
2024-03-20 22:39:51 -0300

    tela cadastro inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TelaCadastroModule } from './pacientes/tela-cadastro/tela-cadastro.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TelaCadastroModule, MatToolbarModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
