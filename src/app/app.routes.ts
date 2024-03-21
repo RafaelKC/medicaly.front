ac2edf277351e8609498050b9ae25a8f8cf07124
Victor Moro <morovictor733@gmail.com>
2024-03-21 00:02:15 -0300

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../components/login/login.component';
+import { TelaCadastroComponent } from './pacientes/tela-cadastro/tela-cadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: TelaCadastroComponent},
+    {path:"login", component:LoginComponent}
+];
---
+++ src/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule,
+    MatInputModule,RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+ 
+  constructor(private _fb: FormBuilder){}
+  loginForm = this._fb.group({
+    email: '',
+    password: ''
+  })
+
+  onSubmit(){
+    console.log("Submit form", this.loginForm.value)
+  }
+}
