8e480c3d0264ee583ea089b9696c73b8c629abca
Luis Otavio Silva Santos <otavio.l@pucpr.edu.br>
2024-03-27 21:35:01 -0300

    sidenav ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,56 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {TelaCadastroModule} from './nao-autenticado/pacientes/tela-cadastro/tela-cadastro.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {CommonModule} from "@angular/common";
 import {MatIconButton} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatButtonModule} from '@angular/material/button';
+
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TelaCadastroModule, MatToolbarModule, MatIconModule, CommonModule, MatIconButton],
+  imports: [
+    RouterOutlet, 
+    TelaCadastroModule, 
+    MatToolbarModule, 
+    MatIconModule, 
+    CommonModule, 
+    MatIconButton,
+    MatSidenavModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
+  public svg = `<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+  width="256.000000pt" height="256.000000pt" viewBox="0 0 256.000000 256.000000"
+  preserveAspectRatio="xMidYMid meet">
+ 
+ <g transform="translate(0.000000,256.000000) scale(0.100000,-0.100000)"
+ fill="#000000" stroke="none">
+ </g>
+ </svg>`
+
+  /*constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer){
+    iconRegistry.addSvgIcon(
+      'medicaly-logo',
+      sanitizer.bypassSecurityTrustResourceUrl(this.svg))
+  }*/
   title = 'medicaly.front';
+
+  constructor(private router: Router){
+
+  }
+  public navegar(link: string): void{
+    this.router.navigate([link])
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {TelaCadastroComponent} from './nao-autenticado/pacientes/tela-cadastro/tela-cadastro.component';
 import {LoginComponent} from "./nao-autenticado/login/login.component";
+import { ContatoComponent } from './contato/contato.component';
 
 export const routes: Routes = [
     {path: "cadastro-paciente", component: TelaCadastroComponent},
-    {path:"login", component:LoginComponent}
+    {path:"login", component:LoginComponent},
+    {path:"contato", component:ContatoComponent}
 ];
