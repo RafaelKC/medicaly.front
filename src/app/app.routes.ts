e735ead2599dd06250b8eda25a4e1eab4f19ac5e
Victor Moro <morovictor733@gmail.com>
2024-03-27 22:17:52 -0300

    inicio commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { TelaCadastroComponent } from './pacientes/tela-cadastro/tela-cadastro.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 export const routes: Routes = [
-    {path: "", component: TelaCadastroComponent},
+    {path: "", component: InicioComponent},
+    {path: "cadastro", component: TelaCadastroComponent},
     {path:"login", component:LoginComponent}
 ];
