1d1d7e1f120d9e7d8302cdede32c8b5147118e99
RafaelKC <rafaelkaua97@gmail.com>
2024-03-30 10:45:46 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,23 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
 import {TelaCadastroModule} from './nao-autenticado/pacientes/tela-cadastro/tela-cadastro.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+import {MatIconModule} from '@angular/material/icon';
 import {CommonModule} from "@angular/common";
-import {MatIconButton} from "@angular/material/button";
+import {MatButtonModule, MatIconButton} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatButtonModule} from '@angular/material/button';
-
-import { Router, RouterModule } from '@angular/router';
-import { ContatoModule } from './contato/contato.module';
+import {ContatoModule} from './contato/contato.module';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    TelaCadastroModule, 
-    MatToolbarModule, 
-    MatIconModule, 
-    CommonModule, 
+    RouterOutlet,
+    TelaCadastroModule,
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule,
     MatIconButton,
     MatSidenavModule,
     MatButtonModule,
@@ -36,7 +33,7 @@ export class AppComponent {
   public svg = `<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
   width="256.000000pt" height="256.000000pt" viewBox="0 0 256.000000 256.000000"
   preserveAspectRatio="xMidYMid meet">
- 
+
  <g transform="translate(0.000000,256.000000) scale(0.100000,-0.100000)"
  fill="#000000" stroke="none">
  </g>
