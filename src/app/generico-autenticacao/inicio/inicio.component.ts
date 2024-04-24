1239691126d18ebe17f80c5e83e7affa766cf0fe
RafaelKC <rafaelkaua97@gmail.com>
2024-04-24 20:00:03 -0300

    Ajustes

--- src/app/generico-autenticacao/inicio/inicio.component.ts
+++ src/app/generico-autenticacao/inicio/inicio.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AnexosService} from "../../../tokens/services/anexos.service";
 
 @Component({
   selector: 'app-inicio',
@@ -8,19 +7,4 @@ import {AnexosService} from "../../../tokens/services/anexos.service";
 })
 export class InicioComponent {
 
-  constructor(private anexosService: AnexosService) { }
-
-  public upload(event: Event): void {
-    if (event == null) return;
-    const element = event.currentTarget as HTMLInputElement;
-    if (element.files == null || element.files.length == 0) return;
-
-    const file = element.files[0];
-
-    if (file) {
-      this.anexosService.upload(file).subscribe({
-        next: (response: Response) => console.log(response)
-      })
-    }
-  }
 }
--- src/app/generico-autenticacao/inicio/inicio.module.ts
+++ src/app/generico-autenticacao/inicio/inicio.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {InicioComponent} from "./inicio.component";
 import {RouterModule, Routes} from "@angular/router";
-import {HttpMedicalyModule} from "../../http-medicaly.module";
-import {AnexosService} from "../../../tokens/services/anexos.service";
 
 const routes: Routes = [
   { path: '', component: InicioComponent }
@@ -11,7 +9,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [InicioComponent],
   exports: [RouterModule],
-  imports: [RouterModule.forChild(routes), HttpMedicalyModule],
-  providers: [AnexosService]
+  imports: [RouterModule.forChild(routes)],
 })
 export class InicioModule { }
+
