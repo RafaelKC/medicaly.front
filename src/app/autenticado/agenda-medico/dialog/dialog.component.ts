ddf83735ea1e64e347deee67262f07625f1adb05
Luis <luotasss@gmail.com>
2024-05-24 21:23:43 -0300

    mofalll

--- src/app/autenticado/agenda-medico/dialog/dialog.component.ts
+++ src/app/autenticado/agenda-medico/dialog/dialog.component.ts
@@ -1,18 +1,26 @@
-import {Component, Inject} from '@angular/core';
+import {Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle} from "@angular/material/dialog";
 import {ProcedimentoOutput} from "../../../../tokens/models/procedimento-output";
 import {EventImpl} from "@fullcalendar/core/internal";
 import {
-    MatCard,
-    MatCardContent,
-    MatCardHeader,
-    MatCardImage,
-    MatCardSubtitle,
-    MatCardTitle
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle
 } from "@angular/material/card";
-import {DatePipe} from "@angular/common";
-import {MatButton} from "@angular/material/button";
+import {CommonModule, DatePipe} from "@angular/common";
+import {MatButton, MatFabButton} from "@angular/material/button";
 import {DialogRef} from "@angular/cdk/dialog";
+import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {CdkTextareaAutosize} from "@angular/cdk/text-field";
+import {UploadIcon} from "primeng/icons/upload";
+import {ProcedimentoService} from "../procedimento.service";
+import {ProcedimentoInput} from "../../../../tokens/models/procedimento";
+import {StatusProcedimento} from "../../../../tokens/enums/status-procedimento";
 
 
 export interface DialogData {
@@ -33,7 +41,15 @@ export interface DialogData {
     MatCardSubtitle,
     MatCardTitle,
     DatePipe,
-    MatButton
+    MatButton,
+    MatIcon,
+    MatFabButton,
+    CommonModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    CdkTextareaAutosize,
+    UploadIcon
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
@@ -42,7 +58,17 @@ export interface DialogData {
 
 
 export class DialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData, public dialogref: DialogRef<DialogComponent>) {}
+  showFinalizar: boolean = false;
+  protected srcResult: any;
+
+
+
+  @ViewChild("uploadInput") uploadInput: ElementRef<HTMLInputElement>;
+  uploadClicado: boolean = true;
+  protected fileName: string;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData,
+              public dialogref: DialogRef<DialogComponent>,
+              public procedimentoService: ProcedimentoService) {}
 
   genero(genero: number) {
     if (genero==0){return 'Masculino'}
@@ -61,5 +87,54 @@ export class DialogComponent {
     if (status==0){return 'Ativo'}
     if (status==1){return 'Em Andamento'}
     return "Cancelado"
+  }
+
+  showFinalizarDiv(){
+    this.showFinalizar = !this.showFinalizar
+  }
+
+  artigo(tipoProcedimento: number) {
+    if (tipoProcedimento==2){return 'o'}
+    return "a"
+  }
+
+  limitCaracteres(s: string) {
+    if (s.length > 17) {
+      return s.substring(0, 17) + '...';
+    } else {
+      return s;
+    }
+  }
+
+  onFileSelected() {
+    const inputNode: any = this.uploadInput.nativeElement;
+    this.fileName = this.limitCaracteres(inputNode.files[0].name);
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+      };
+
+      reader.readAsArrayBuffer(inputNode.files[0]);
+      this.uploadClicado = !this.uploadClicado
+
+    }
+  }
+
+  uploadClicked(event: Event) {
+
+    event.preventDefault()
+    if(!this.uploadClicado){
+      this.uploadClicado = !this.uploadClicado
+      this.fileName=''
+    }else {
+      this.uploadInput.nativeElement.click()
+    }
+  }
+
+  cancelarProcedimento(id: string, p: ProcedimentoInput) {
+    p.status = StatusProcedimento.Cancelado
+    this.procedimentoService.update(id, p).subscribe()
   }
 }
---
+++ src/app/autenticado/agenda-medico/procedimento.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {ProcedimentoInput} from "../../../tokens/models/procedimento";
+import {Observable} from "rxjs";
+import {ensureTrailingSlash} from "../../../tokens/functions/ensure-trailing-slash";
+import {environment} from "../../../environments/environment";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProcedimentoService {
+  public get basePath(): string {
+    return `${ensureTrailingSlash(environment.apiUrl)}procedimentos`;
+  }
+
+  constructor(private http: HttpClient ) { }
+
+  public update(id: string, input: ProcedimentoInput): Observable<any> {
+    console.log('tentando updatar')
+    return this.http.put<any>(this.basePath + `/${id}`, input);
+  }
+
+}
